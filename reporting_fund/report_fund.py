4b7c58b41a8ac6bff67aed6657cfd5bf18a4c58a
Felix Zumstein <fzumstein@gmail.com>
2019-10-12 13:21:48 +0200

    added fund reporting sample

---
+++ reporting_fund/report_fund.py
@@ -0,0 +1,26 @@
+import pickle
+import datetime as dt
+from xlwings_reports import create_report  # not part of the open-source xlwings package
+
+# Get your data via SQL, APIs, text files, Excel files etc.
+with open('data.pickle', 'rb') as f:
+    historical_perf, asset_allocation, top_ten_holdings, calendar_year_tot_ret, tot_ret = pickle.load(f)
+
+# Manipulate your data (with pandas, obviously)
+historical_perf = historical_perf.resample('M').last()
+
+# Collect all data
+fmt = '%e %b %Y'
+data = dict(perf_start_date=dt.datetime(2009, 1, 1).strftime(fmt),
+            perf_end_date=dt.date.today().strftime(fmt),
+            reference_date=dt.date.today().strftime(fmt),
+            total_net_assets=123,
+            historical_perf=historical_perf,
+            asset_allocation=asset_allocation,
+            top_ten_holdings=top_ten_holdings,
+            tot_ret=tot_ret,
+            calendar_year_tot_ret=calendar_year_tot_ret,
+            )
+
+# Create the Excel report
+wb = create_report('fund_template.xlsx', 'fund_report.xlsx', **data)
