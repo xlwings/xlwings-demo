530f46f0d39b15ef6a646a8a1494c732af40781a
Felix Zumstein <fzumstein@gmail.com>
2019-10-14 11:01:52 +0200

    added config

--- reporting_fund/fund_template.py
+++ reporting_fund/fund_template.py
@@ -1,19 +1,33 @@
 import os
 import pickle
 import datetime as dt
+import xlwings as xw
 from xlwings_reports import create_report  # not part of the open-source xlwings package
 
 
 def main():
+    # Files
+    template = xw.Book.caller()
+    template_path = template.fullname
+    report_path = os.path.join(os.path.dirname(template.fullname), 'fund_report.xlsx')
+
     # Get your data via SQL, APIs, text files, Excel files etc.
     with open(os.path.join(os.path.dirname(__file__), 'data.pickle'), 'rb') as f:
         historical_perf, asset_allocation, top_ten_holdings, calendar_year_tot_ret, tot_ret = pickle.load(f)
 
-    # Manipulate your data (with pandas, obviously)
+    # Data wrangling (with pandas, obviously)
     historical_perf = historical_perf.resample('M').last()
 
+    # Configuration (optional)
+    date_format = template.sheets['Config']['date_format'].value
+    if date_format == 'UK':
+        fmt = '%e %b %Y'
+    elif date_format == 'US':
+        fmt = '%b %e, %Y'
+    else:
+        fmt = '%e %b %Y'
+
     # Collect all data
-    fmt = '%e %b %Y'
     data = dict(perf_start_date=dt.datetime(2009, 1, 1).strftime(fmt),
                 perf_end_date=dt.date.today().strftime(fmt),
                 reference_date=dt.date.today().strftime(fmt),
@@ -26,10 +40,10 @@ def main():
                 )
 
     # Create the Excel report
-    template = os.path.join(os.path.dirname(__file__), 'fund_template.xlsx')
-    report = os.path.join(os.path.dirname(__file__), 'fund_report.xlsx')
-    wb = create_report(template, report, **data)
+    wb = create_report(template_path, report_path, **data)
 
 
 if __name__ == '__main__':
+    # This part is to run the script directly from Python, not via Excel
+    xw.Book(os.path.join(os.path.dirname(__file__), 'fund_template.xlsx')).set_mock_caller()
     main()
