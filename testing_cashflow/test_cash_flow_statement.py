8fb840e9a529c28f99f8ecee3f995eb90808f890
Felix Zumstein <fzumstein@gmail.com>
2019-10-23 19:45:12 +0200

    moved fixture into utils

--- testing_cashflow/test_cash_flow_statement.py
+++ testing_cashflow/test_cash_flow_statement.py
@@ -1,26 +1,5 @@
-import os
-import shutil
-import xlwings as xw
-from pytest import approx, fixture
-
-
-@fixture(scope="module")
-def book():
-    # Setup
-    filepath = 'cash_flow_statement.xlsx'
-    test_filepath = 'test_' + filepath
-    shutil.copyfile(filepath, test_filepath)
-    app = xw.App(visible=False)
-    wb = app.books.open(test_filepath)
-    yield wb
-
-    # Teardown
-    wb.save()
-    app.quit()
-    try:
-        os.remove(test_filepath)
-    except:
-        pass
+from pytest import approx
+from utils import book
 
 
 def test_cash_flow_formula_integrity(book):
@@ -29,4 +8,3 @@ def test_cash_flow_formula_integrity(book):
     sheet['B3:M3'].value = 10
     sheet['B4:M4'].value = -5
     assert sheet['M5'].value == approx(160)
-
