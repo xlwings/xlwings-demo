569917237c70a6de2dfed227ea78012b9588522a
Felix Zumstein <fzumstein@gmail.com>
2020-01-14 10:52:09 +0100

    formatting

--- reporting/report_template.py
+++ reporting/report_template.py
@@ -12,13 +12,15 @@ def main():
     template_path = template.fullname
     report_path = os.path.join(os.path.dirname(template.fullname), 'report.xlsx')
 
+    # Matplatlib
     fig = Figure(figsize=(4, 3))
     ax = fig.add_subplot(111)
     ax.plot([1, 2, 3, 4, 5])
 
+    # Pandas DataFrame
     perf_data = pd.DataFrame(index=['r1', 'r1'],
-                            columns=['c0', 'c1'],
-                            data=[[1., 2.], [3., 4.]])
+                             columns=['c0', 'c1'],
+                             data=[[1., 2.], [3., 4.]])
 
     wb = create_report(template_path,
                        report_path,
@@ -26,6 +28,7 @@ def main():
                        perf_data=perf_data,
                        logo=Image.open(os.path.join(os.path.dirname(template.fullname), 'xlwings.jpg')),
                        fig=fig)
+
 
 if __name__ == '__main__':
     # This part is to run the script directly from Python, not via Excel
